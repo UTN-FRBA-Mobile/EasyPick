01401501bc2e4929d6fa20c46ab66ef6ed40ca7e
pdroven <44630293+pdroven@users.noreply.github.com>
2020-05-02 16:05:27 -0300

    Se agrega API de catalogo

---
+++ API/get_catalogo.php
@@ -0,0 +1,20 @@
+<?php
+
+$id=$_GET['id'];
+
+$catalogo='[{"codigo":"7793982832983", "precio": "80.20", "foto":"http://gardinia.online/api/img/coca.png","descripcion:":"Coca Cola 600 ml","comentarios":"Se sirve fria"},{"codigo":"779398283333", "precio": "80.20", "foto":"http://gardinia.online/api/img/pepsi.jpg","descripcion:":"Pepsi 600 ml","comentarios":"Se sirve fria"},
+{"codigo":"779398283222", "precio": "80.20", "foto":"http://gardinia.online/api/img/sprite.jpg","descripcion:":"Sprite 600 ml","comentarios":"Se sirve fria"},
+{"codigo":"77939828324545", "precio": "80.20", "foto":"http://gardinia.online/api/img/sprite_diet.png","descripcion:":"Sprite DIET 600 ml","comentarios":"Se sirve fria"},
+{"codigo":"7793982832342", "precio": "180.00", "foto":"http://gardinia.online/api/img/hamburguesa.jpg","descripcion:":"Hamburguesa Completa","comentarios":"Lechuga,tomar y queso"},
+{"codigo":"779398255553", "precio": "120.00", "foto":"http://gardinia.online/api/img/papas.png","descripcion:":"Papas Fritas","comentarios":"Con sale"},
+{"codigo":"7793982845454", "precio": "220.00", "foto":"http://gardinia.online/api/img/hamburguesa2.png","descripcion:":"Hamburguesa Casera","comentarios":"Pan de centeno, tomat, queso"},
+{"codigo":"7793982843432", "precio": "180.00", "foto":"http://gardinia.online/api/img/chop_rubia.png","descripcion:":"Chop IPA 500 ml","comentarios":"Se sirve fria"},
+{"codigo":"779398423432", "precio": "150.00", "foto":"http://gardinia.online/api/img/corona.png","descripcion:":"Corona 430 ml","comentarios":"Se sirve fria"},
+{"codigo":"779398444443", "precio": "180.00", "foto":"http://gardinia.online/api/img/pinta_rubia.png","descripcion:":"Pinta Rubia 500 ml","comentarios":"Se sirve fria"}]';
+
+
+echo $catalogo;
+
+
+
+?>
