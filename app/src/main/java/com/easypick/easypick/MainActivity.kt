a2d3b9824d841680d71bc6eb0f9840a055f6a466
pdroven <pedrodroven@hotmail.com>
2020-05-14 09:23:48 -0300

    Diseño de aplicacion

--- app/src/main/java/com/easypick/easypick/MainActivity.kt
+++ app/src/main/java/com/easypick/easypick/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
 
 class MainActivity : AppCompatActivity(){
