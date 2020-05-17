07fa36b4cf461e8dc8f46c14bc0b5b4e918c4660
Lucas Gilone <lucas.saka21@gmail.com>
2020-05-17 01:03:57 -0300

    Fix main fragment layout. Add scroll views for fragments. Fix status bar color

--- app/src/main/java/com/easypick/easypick/Principal.kt
+++ app/src/main/java/com/easypick/easypick/Principal.kt
@@ -20,6 +20,7 @@ class Principal :  AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+        replaceFragment(FragmentHome())
         btn_next.setOnClickListener { replaceFragment(PagoFragment()) }
 
     }
