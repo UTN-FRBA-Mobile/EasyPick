ce6ccdfa0f596dcfeec58f2c57a0933c339bdb8c
pdroven <pedrodroven@hotmail.com>
2020-06-08 14:40:10 -0300

    Barra de loading2

--- app/src/main/java/com/easypick/easypick/fragments/FragmentHome.kt
+++ app/src/main/java/com/easypick/easypick/fragments/FragmentHome.kt
@@ -1,27 +1,47 @@
 package com.easypick.easypick.fragments
 
+import android.Manifest.*
 import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.icu.lang.UCharacter
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.SyncStateContract.Helpers.insert
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat.checkSelfPermission
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.easypick.easypick.Locales
 
-import com.easypick.easypick.R
 import com.easypick.easypick.R.*
 
 import com.easypick.easypick.adapters.AdaptadorLocales
+import kotlinx.android.synthetic.main.activity_principal.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_producto.*
+import java.security.Permission
+import java.security.Permissions
+import java.util.jar.Manifest
 
 
 class FragmentHome : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
+    private val PERMISION_CODE:Int =1000
+    private val IMAGE_CAPTURE_CODE:Int=1001
+    var image_rui: Uri?= null
+
 
     private val locales = listOf(
     Locales(titulo = "Pizzeria", detalle = "Pizzeria Vegana, más de 14 sabores", foto = drawable.resto1) ,
@@ -34,9 +54,13 @@ class FragmentHome : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         retainInstance = true
     }
+
+
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -55,7 +79,12 @@ class FragmentHome : Fragment() {
                 adapter = AdaptadorLocales(locales)
             }
 
-    }
+
+          
+
+
+
+        }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -78,3 +107,5 @@ class FragmentHome : Fragment() {
 
 
 }
+
+
