303a58e1da847ad0406231f14f3b4a89e760c30c
nrmerlis <nicoxeneize.10@gmail.com>
2020-07-05 19:19:56 -0300

    - Agrego clase Remarked!

---
+++ app/src/main/java/com/easypick/easypick/model/Remarked.kt
@@ -0,0 +1,4 @@
+package com.easypick.easypick.model
+
+class Remarked (var image: String, var id: Long, var product: Producto) {
+}
--- app/src/main/java/com/easypick/easypick/retroFit/Gateway.kt
+++ app/src/main/java/com/easypick/easypick/retroFit/Gateway.kt
@@ -2,6 +2,7 @@ package com.easypick.easypick.retroFit
 
 import com.easypick.easypick.model.Category
 import com.easypick.easypick.model.Producto
+import com.easypick.easypick.model.Remarked
 import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
@@ -17,4 +18,7 @@ interface Gateway {
 
     @GET("/stores/{id}/categories")
     fun getCategoryByStoreId(@Path("id") id:Long): Call<List<Category>>
+
+    @GET("/stores/{id}/remarkeds")
+    fun getRemarkedsByStoreId(@Path("id") id:Long): Call<List<Remarked>>
 }
