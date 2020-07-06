bd2a0e3dfc784a8357df2aa0e86e21147e7652c2
patoadrigomez <gomezpatricioarian@gmail.com>
2020-07-06 10:33:20 -0300

    algunos arreglos

--- app/src/main/java/com/easypick/easypick/retroFit/Gateway.kt
+++ app/src/main/java/com/easypick/easypick/retroFit/Gateway.kt
@@ -13,7 +13,7 @@ import java.util.*
 interface Gateway {
     @GET("api.php")
     fun getProductoByCategoryId(@Query("store") store: Long,
-                                @Query("categoria") categoria: Long): Call<List<Producto>>
+                                @Query("categoria") categoria: Int): Call<List<Producto>>
 
     @GET("/stores/{id}/categories")
     fun getCategoryByStoreId(@Path("id") id:Long): Call<List<Category>>
